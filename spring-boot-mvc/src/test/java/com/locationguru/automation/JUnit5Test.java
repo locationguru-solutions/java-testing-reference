3fbf3da562d893532cd936ceb837e29a5b92a710
Kedar Joshi <kedar.joshi@locationguru.com>
2019-07-04 14:08:35 +0530

    Added JUnit 5 sample test class

---
+++ spring-boot-mvc/src/test/java/com/locationguru/automation/JUnit5Test.java
@@ -0,0 +1,33 @@
+package com.locationguru.automation;
+
+import org.junit.jupiter.api.*;
+
+public class JUnit5Test
+{
+	@BeforeAll
+	public static void initializeAll()
+	{
+
+	}
+
+	@BeforeEach
+	public void initialize()
+	{
+
+	}
+
+	@AfterEach
+	public void destroy()
+	{
+
+	}
+
+	@AfterAll
+	public static void destroyAll()
+	{
+
+	}
+}
+
+
+
